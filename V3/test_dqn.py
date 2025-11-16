655ddc9ae9606eecb054d3a4f89ae6148cb5e38c
Amik05 <amatianiu@gmail.com>
2025-11-16 10:44:18 -0800

    add V3, DQN implementation

---
+++ V3/test_dqn.py
@@ -0,0 +1,27 @@
+import torch
+from car_lidar_env import CarLidarEnv
+from dqn_agent import DQNAgent
+
+env = CarLidarEnv(render_mode="human")
+
+# Get observation/action size
+obs, _ = env.reset()
+obs_dim = len(obs)
+action_dim = env.action_space.n
+
+# Load agent
+agent = DQNAgent(obs_dim, action_dim)
+agent.q_net.load_state_dict(torch.load("dqn_car.pth"))
+agent.q_net.eval()
+
+done = False
+
+while not done:
+    with torch.no_grad():
+        s = torch.FloatTensor(obs).unsqueeze(0)
+        action = agent.q_net(s).argmax().item()
+
+    obs, reward, terminated, truncated, _ = env.step(action)
+    done = terminated or truncated
+
+    env.render()
